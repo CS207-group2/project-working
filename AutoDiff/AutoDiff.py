fa89ff530df52dc4f313197011fe5b2e0b58e30f
Maleah <mfekete@berkeley.edu>
2018-11-04 12:34:51 -0500

    AutoDiff can now be used with multivariable functions. get_der can accept lists, lists of lists, or a number as arguements

--- AutoDiff/AutoDiff.py
+++ AutoDiff/AutoDiff.py
@@ -1,16 +1,48 @@
 from Dual.dual import Dual
-
+import inspect
 class AutoDiff:
     def __init__(self, fn):
         self.fn = fn
 
     def get_der(self, val):
-        if (isinstance(val,list)):
-            ders = []
+        """ Returns derivatives of the function evaluated at values given.
+        
+        inputs
+        ------
+        val : single number, a list of numbers, or a list of lists
+
+        returns
+        ------
+
+        """
+        ders = []
+        sig = inspect.signature(self.fn)
+        l = len(list(sig.parameters))
+        if l >= 2:
+            #for list of lists, each list evaluated at different variables
+            if any(isinstance(el, list) for el in val) is True:  
+                list_der = []
+                for p in val:
+                    list_der.append(self.get_der(p))
+                return list_der
+            elif l != len(val):
+                raise Exception('Function requires {} values that correspond to the multiple variables'.format(l))
+            else:
+                #for a list of numbers, evaluated at different variables.
+                for i in range(l): 
+                    new_val = val.copy()
+                    new_val[i] = Dual(new_val[i])
+                    ders.append(self.fn(*new_val).der)
+                return ders
+        #for a list of numbers, evaluated at a single variable.
+        if (isinstance(val,list)): 
             for v in val:
-                x = Dual(v)
-                ders.append(self.fn(x).der)
+                a = Dual(v)
+                ders.append(self.fn(a).der)
             return ders
         else:
-            x = Dual(val)
-            return self.fn(x).der
+            a = Dual(val)
+            return self.fn(a).der
+
+
+       
