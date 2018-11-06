df4729b4143accdc0d4e011a6e2593790c5673fb
Phoebe Wong <phoebe.wong@berkeley.edu>
2018-11-06 17:15:48 -0500

    Add log, exponential and change to numpy

--- AutoDiff/admath/admath.py
+++ AutoDiff/admath/admath.py
@@ -1,18 +1,59 @@
 from ..dual.dual import Dual
-import math
+#import math
+import numpy as np
 
 def sin(x):
     if (isinstance(x,Dual)):
-        x.der = math.cos(x.val)*x.der        
-        x.val = math.sin(x.val)
+        x.der = np.cos(x.val)*x.der
+        x.val = np.sin(x.val)
         return x
     else:
-        return math.sin(x)
+        return np.sin(x)
 
 def cos(x):
+    """Calculate cosine of the input
+        
+        Keyword arguments:
+        x -- a real number or a dual number
+        
+        Return:
+        the cosine value
+    """
     if (isinstance(x,Dual)):
-        x.der = -1 * math.sin(x.val)*x.der
-        x.val = math.cos(x.val)
+        x.der = -1 * np.sin(x.val)*x.der
+        x.val = np.cos(x.val)
         return x
     else:
-        return math.cos(x)
+        return np.cos(x)
+
+def log(x):
+    """Calculate the natural log of the input
+        
+        Keyword arguments:
+        x -- a real number or a dual number
+        
+        Return:
+        the natural log value
+    """
+    if (isinstance(x,Dual)):
+        x.der = (1/x.val)*x.der
+        x.val = np.log(x.val)
+        return x
+    else:
+        return np.log(x)
+
+def exp(x):
+    """Calculate the exponential of the input
+        
+        Keyword arguments:
+        x -- a real number or a dual number
+        
+        Return:
+        the exponential value
+        """
+    if (isinstance(x,Dual)):
+        x.der = *x.der
+        x.val = np.cos(x.val)
+        return x
+    else:
+        return np.cos(x)
