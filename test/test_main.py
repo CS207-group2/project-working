3aaf08d14bbcb49553196ae14678ebc75bec88df
unknown <groever@g.harvard.edu>
2018-11-07 13:26:52 -0800

    Add interface

--- test/test_main.py
+++ test/test_main.py
@@ -3,12 +3,9 @@ from autodiff.dual import dual
 import autodiff.admath.admath as math
 import numpy as np
 
-def sin_fn(x):
-    return math.sin(x)
-
 def test__sin_fn__():
     x = dual.Dual(0)
-    f = sin_fn(x)
+    f = math.sin(x)
     assert f.val == 0
     assert f.der == 1
     
