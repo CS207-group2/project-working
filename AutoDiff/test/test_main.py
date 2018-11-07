dabe8c8673311016f4066140663036d33527ac1e
Phoebe Wong <phoebe.wong@berkeley.edu>
2018-11-06 22:09:33 -0500

    Add test in test_main

--- AutoDiff/test/test_main.py
+++ AutoDiff/test/test_main.py
@@ -17,3 +17,19 @@ print(ad_square.get_der([1,2]))
 ad_sin = AutoDiff(sin_fn)
 print(ad_sin.get_der(2))
 # Evaluates derivative of sin(5x)+2x^2 when x = 2
+
+def cos_fn(x): # trial cosine function to feed into AutoDiff
+    return admath.cos(-3*x) + 3*x
+
+ad_cos = AutoDiff(cos_fn)
+print(ad_cos.get_der([2,3]))
+
+#
+##print(math.sin([4,5]))
+## Evaluates derivative of sin(5x)+2x^2 when x = 2, 5
+#
+#a = np.array([3,4,5])
+#a = a*2
+#
+#
+#print(sin_fn(4))
